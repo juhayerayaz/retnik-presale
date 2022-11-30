c65aa2be791cbd96912a42d4937f2f631fbd0b23
juhayerayaz <aiazj97@gmail.com>
2022-12-01 04:31:08 +0600

    notfound page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Signup from './Components/Login/Signup';
+import NotFound from './Components/Notfound/Notfound';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   );
